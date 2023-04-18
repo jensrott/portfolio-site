38b7d24d2648d38ca8de791d7597d7113c6bb64e
Jens Rottiers <jens.rottiers@scarlet.be>
2023-04-18 14:19:13 +0200

    Open browser on start

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
     return {
+        server: {
+            open: true,
+        },
         build: {
             outDir: 'build',
         },
